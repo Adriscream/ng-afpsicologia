4d02b45437fc0a96da2648c5187aac3fb03dc049
Adrián López <adrianislopezis@gmail.com>
2023-04-28 14:22:01 +0200

    .

--- api/src/packages/firestore/firestore.config.ts
+++ api/src/packages/firestore/firestore.config.ts
@@ -2,15 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyCvIW1kcQodY8c46iQJpyptZZABRW_-1BI',
-  authDomain: 'ng-afpsicologia.firebaseapp.com',
-  projectId: 'ng-afpsicologia',
-  storageBucket: 'ng-afpsicologia.appspot.com',
-  messagingSenderId: '1090736482366',
-  appId: '1:1090736482366:web:cebe4cf49404714adb496f',
-  measurementId: 'G-SEMVPZCM0Z',
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
