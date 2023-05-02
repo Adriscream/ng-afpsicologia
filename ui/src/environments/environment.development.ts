323c492209ae4c085f55e8f648bca84c1fcdbd2d
Adrián López <adrianislopezis@gmail.com>
2023-05-03 00:37:43 +0200

    fix

--- ui/src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  appName: 'DealX DEV',
-  baseUrl: 'http://localhost:3000/',
-} as const;
--- ui/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  appName: 'DealX',
-  baseUrl: 'http://localhost:3000/',
-} as const;
