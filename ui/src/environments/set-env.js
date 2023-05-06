881726eb8f3dec5eb4a396f16e52f6c983a6ab3a
Adrián López <adrianislopezis@gmail.com>
2023-05-06 21:55:21 +0200

    feat(ci): angular environment files

---
+++ ui/src/environments/set-env.js
@@ -0,0 +1,30 @@
+const { writeFile } = require('fs');
+const { name, version } = require('../../package.json');
+
+const targetPath = './src/environments/environment.ts';
+require('dotenv').config({
+  path: './src/environments/.env',
+});
+debugger;
+const envConfigFile = `export const environment = {
+    baseUrl: '${process.env.BASE_URL}',
+    imgCDNPublicKey: '${process.env.IMG_CDN_PUBLIC_KEY}',
+    imgCDNUrlEndpoint: '${process.env.IMG_CDN_URL_ENDPOINT}',
+    imgCDNAuthEndpoint: '${process.env.IMG_CDN_AUTH_ENDPOINT}',
+    firestoreApiKey: '${process.env.FIRESTORE_API_KEY}',
+    firestoreAuthDomain: '${process.env.FIRESTORE_AUTH_DOMAIN}',
+    firestoreProjectId: '${process.env.FIRESTORE_PROJECT_ID}',
+    firestoreStorageBucket: '${process.env.FIRESTORE_STORAGE_BUCKET}',
+    firestoreSenderId: '${process.env.FIRESTORE_SENDER_ID}',
+    firestoreAppId: '${process.env.FIRESTORE_APP_ID}',
+    firestoreMeasurementId: '${process.env.FIRESTORE_MEASUREMENT_ID}',
+    name: '${name}',
+    version: '${version}'
+};
+`;
+
+writeFile(targetPath, envConfigFile, 'utf8', (err) => {
+  if (err) {
+    return console.log(err);
+  }
+});
