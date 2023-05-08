14acf7402242ac2fdcee765d4327bf8e189fd6ad
Adrián López <adrianislopezis@gmail.com>
2023-05-08 02:15:17 +0200

    test: ci

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ImagekitioAngularModule } from 'imagekitio-angular';
-import { environment } from 'src/environments/environment';
 
 import { AppComponent } from './app.component';
 import { AfHttpModule } from './common/http/http.module';
-// import { ImageCDNModule } from './common/image-cdn/image-cdn.module';
+import { ImageCDNModule } from './common/image-cdn/image-cdn.module';
 import { StateModule } from './common/store/state.module';
 import { AppRoutingModule } from './routing/app-routing.module';
 
@@ -18,12 +16,7 @@ import { AppRoutingModule } from './routing/app-routing.module';
     AfHttpModule,
     StateModule,
     AppRoutingModule,
-    ImagekitioAngularModule.forRoot({
-      publicKey: environment.imgCDNPublicKey,
-      urlEndpoint: environment.imgCDNUrlEndpoint,
-      authenticationEndpoint: environment.imgCDNAuthEndpoint,
-    }),
-    // ImageCDNModule,
+    ImageCDNModule,
   ],
   bootstrap: [AppComponent],
 })
